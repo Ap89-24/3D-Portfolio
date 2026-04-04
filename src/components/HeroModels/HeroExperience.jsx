13f06da4cad76620fd635d358f43514edb6b6699
Ap89-24 <aman082199@gmail.com>
2026-04-04 16:55:02 +0530

    building 3d model page

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,18 +1,33 @@
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
+import { useMediaQuery } from 'react-responsive'
+import { ModelRoom } from './ModelRoom';
+import HeroLights from './HeroLights';
 
 
 const HeroExperience = () => {
+
+  const isTablet = useMediaQuery({query: "(max-width: 1024px)"});
+  const isMobile = useMediaQuery({query: "(max-width: 768px)"});
+
   return (
     <Canvas camera={{position: [0,0,15] , fov: 45}}>
-      <ambientLight intensity={1} />
-      <directionalLight position={[5,5,5]} intensity={4} />
-      <OrbitControls />
-
-      <mesh>
-        <boxGeometry args={[2,2,2]} />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+      <OrbitControls 
+      enablePan={false}
+      enableZoom={!isTablet}
+      maxDistance={25}
+      minDistance={6}
+      maxPolarAngle={Math.PI / 2}
+      minPolarAngle={Math.PI / 5}
+      />
+      <HeroLights />
+      <group
+      scale={isMobile ? 0.7 : 1}
+      position={[0 , -3.9 , 0]}
+      rotation={[0 , -Math.PI/5 , 0]}
+      >
+     <ModelRoom />
+      </group>
     </Canvas>
   )
 }

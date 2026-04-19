e7a292b1eed515b907c01be13ef0ea3a1605dfb7
Ap89-24 <aman082199@gmail.com>
2026-04-20 01:26:17 +0530

    building 3d tech stack showcase

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader';
-import { techStackImgs } from '../constants';
+import { techStackIcons } from '../constants';
 import TechIcon from '../components/Models/TechLogos/TechIcon';
 
 const TechStack = () => {
@@ -13,7 +13,7 @@ const TechStack = () => {
          />
 
          <div className="tech-grid">
-            {techStackImgs.map((icon) => (
+            {techStackIcons.map((icon) => (
                 <div key={icon.name} className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg">
                   <div className="tech-card-animated-bg" />    
                   <div className="tech-card-content">

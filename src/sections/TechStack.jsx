074495f480e04f8f04f52664940a69b45a842cb2
Ap89-24 <aman082199@gmail.com>
2026-04-19 23:46:54 +0530

    building 3d tech stack showcase

---
+++ src/sections/TechStack.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import TitleHeader from '../components/TitleHeader'
+
+const TechStack = () => {
+  return (
+    <div id="skills" className="flex-centre section-padding">
+      <div>
+        <TitleHeader
+        title={"My Preferred Tech Stack"}
+        sub={"Skills I Bring to the Table"}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default TechStack
